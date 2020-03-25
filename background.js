eb56971109bc7b433a9c4c138d6a9f0cb32f51a2
chadbartel <chadbartel@users.noreply.github.com>
2020-03-24 18:59:28 -0700

    Add autoplay to Watch Later

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 var targetUrl = "https://www.youtube.com/*";
 var disable_option = "disable_polymer=true";
+var autoplay_option = "autoplay=1"
 
 function rewriteUrl(req) {
-  if (!req.url.includes("disable_polymer"))
+  if (!req.url.includes("disable_polymer") && !req.url.includes("list=WL"))
     if (req.url.includes("?"))
       return { redirectUrl: req.url + "&" + disable_option};
     else
       return { redirectUrl: req.url + "?" + disable_option};
+  else if (!req.url.includes("disable_polymer") && req.url.includes("list=WL"))
+    if (req.url.includes("?"))
+      return { redirectUrl: req.url + "&" + autoplay_option};
+    else
+      return { redirectUrl: req.url + "?" + autoplay_option};
 }
 
 browser.webRequest.onBeforeRequest.addListener(
